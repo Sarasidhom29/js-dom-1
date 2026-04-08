d4b14896a060456de274c9d8f98993c97d026da9
Sara Sidhom <s.sara.29.07.2006@gmail.com>
2026-04-08 21:07:02 +0200

    creata funzione per toggle lampada

--- script.js
+++ script.js
@@ -3,3 +3,18 @@
 const lamp = document.getElementById('lamp-off');
 const powerButton = document.getElementById('turn-on');
 
+function lampSwitch(){
+    
+    if (lamp.src.includes('./img/white_lamp.png')) {
+      lamp.src = './img/yellow_lamp.png';
+      powerButton.textContent = 'Spegni';
+    } else {
+      lamp.src = './img/white_lamp.png';
+      powerButton.textContent = 'Accendi';
+    }
+
+}
+
+
+console.log(lamp);
+console.log(powerButton);
