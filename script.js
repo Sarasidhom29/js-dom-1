78af3c1a2c10fc6e4b916dae34685fb68641647b
Sara Sidhom <s.sara.29.07.2006@gmail.com>
2026-04-08 21:20:58 +0200

    aggiunto console.log nella funzione e addEventListener

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ const lamp = document.getElementById('lamp-off');
 const powerButton = document.getElementById('turn-on');
 
 function lampSwitch(){
-    
-    if (lamp.src.includes('./img/white_lamp.png')) {
+
+    console.log(lamp.src);
+
+    if (lamp.src.includes('img/white_lamp.png')) {
       lamp.src = './img/yellow_lamp.png';
       powerButton.textContent = 'Spegni';
     } else {
@@ -14,6 +16,8 @@ function lampSwitch(){
     }
 
 }
+
+powerButton.addEventListener('click', lampSwitch);
 
 
 console.log(lamp);
